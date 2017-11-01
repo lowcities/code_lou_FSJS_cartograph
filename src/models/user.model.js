411df6b85fd02ab884610e765e1b5d0ad777244a
David Low <musumashae@gmail.com>
2017-11-01 15:21:26 -0400

    disabled bcrypt. noticed that when user add thind to their list. they can no longer authenticate their password when they re-login.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,8 +5,6 @@ const bcrypt = require('bcrypt');
 
 mongoose.Promise = global.Promise;
 
-
-
 // Layout of User profile
 const UserSchema = new mongoose.Schema({
     name: {
@@ -39,37 +37,37 @@ const UserSchema = new mongoose.Schema({
 });
 
 // authenticate input against database documents
-UserSchema.statics.authenticate = function(email, password, callback) {
-    User.findOne({ email: email })
-        .exec(function (error, user) {
-            if (error) {
-                return callback(error);
-            } else if ( !user ) {
-                let err = new Error('User not found');
-                err.status = 401;
-                return callback(err);
-            }
-            console.log(user);
-            bcrypt.compare(password, user.password, function(error, result) {
-                if (result === true) {
-                    return callback(null, user);
-                } else {
-                    return callback();
-                }
-            });
-        })
-}
+// UserSchema.statics.authenticate = function(email, password, callback) {
+//     User.findOne({ email: email })
+//         .exec(function (error, user) {
+//             if (error) {
+//                 return callback(error);
+//             } else if ( !user ) {
+//                 let err = new Error('User not found');
+//                 err.status = 401;
+//                 return callback(err);
+//             }
+//             console.log(user);
+//             bcrypt.compare(password, user.password, function(error, result) {
+//                 if (result === true) {
+//                     return callback(null, user);
+//                 } else {
+//                     return callback();
+//                 }
+//             });
+//         })
+// }
 
 // hash password before saving to database
-UserSchema.pre('save', function(next) {
-    let user = this;
-    bcrypt.hash(user.password, 10, function(err, hash) {
-        if (err) {
-            return next(err);
-        }
-        user.password = hash;
-        next();
-    });
-});
+// UserSchema.pre('save', function(next) {
+//     let user = this;
+//     bcrypt.hash(user.password, 10, function(err, hash) {
+//         if (err) {
+//             return next(err);
+//         }
+//         user.password = hash;
+//         next();
+//     });
+// });
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
